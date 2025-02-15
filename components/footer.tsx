daf05020679adf11eaeec6378dcc2edbc6171651
Jordi Marqués Llaberia <jordi.marquesllaberia@gmail.com>
2025-02-15 20:56:14 +0100

    Add Footer component and integrate it into the main application layout

---
+++ components/footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { XStack, Text, Button } from 'tamagui';
+import Link from 'next/link';
+
+const Footer = () => (
+  <XStack bg="$gray5" p="$4" jc="space-between" ai="center" mt="$6">
+    <Text fontSize="$4">GNU GENERAL PUBLIC LICENSE Version 3</Text>
+    <XStack gap="$4">
+      <Link href="https://linkedin.com" passHref><Button>LinkedIn</Button></Link>
+      <Link href="https://github.com" passHref><Button>GitHub</Button></Link>
+    </XStack>
+  </XStack>
+);
+
+export default Footer;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,15 @@ import type { AppProps } from 'next/app';
 import { TamaguiProvider, Stack } from 'tamagui';
 import tamaguiConfig from '../tamagui.config';
 import Header from '../components/header';
+import Footer from '../components/footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <TamaguiProvider config={tamaguiConfig}>
       <Stack f={1} bg="$background">
-        <Header /> {/* Header se renderiza en TODAS las páginas */}
+        <Header />
         <Component {...pageProps} />
+        <Footer />
       </Stack>
     </TamaguiProvider>
   );
