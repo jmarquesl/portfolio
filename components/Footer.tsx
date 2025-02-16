ccc3077a95bdeb1bc08fc63f97420977ac3743f6
Jordi Marqués Llaberia <jordi.marquesllaberia@gmail.com>
2025-02-16 05:57:14 +0100

    Added icons for social links on footer. Fixed routes for socials

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import {YStack, XStack, Text, Button } from 'tamagui';
+import { YStack, XStack, Text } from 'tamagui';
 import Link from 'next/link';
+import { Linkedin, Github } from 'lucide-react';
 
 const Footer = () => (
-  <YStack p="$4" ai="center" mt="$6" bg="transparent" shadowColor="$shadowColor" shadowRadius="$2" shadowOffset={{ width: 0, height: 2 }}>
-    <XStack gap="$4" mt="$3" justifyContent="space-between" width="100%">
-      <Text fontSize="$4" textAlign="left" fontWeight="bold" alignSelf="center">GNU GENERAL PUBLIC LICENSE Version 3</Text>
+  <YStack p="$4" ai="center" mt="$6">
+    <XStack px="$4" gap="$" mt="$3" justifyContent="space-between" width="100%">
+      <Text fontSize="$4" textAlign="left" fontWeight="bold" alignSelf="center">
+        GNU GENERAL PUBLIC LICENSE Version 3
+      </Text>
       <XStack gap="$4">
-      <Link href="https://linkedin.com" passHref><Button size="$4" variant="outlined" alignSelf="center">LinkedIn</Button></Link>
-      <Link href="https://github.com" passHref><Button size="$4" variant="outlined" alignSelf="center">GitHub</Button></Link>
+        <Link href="https://www.linkedin.com/in/jordi-marqu%C3%A9s-llaberia-11685145/" passHref>
+          <Text>
+            <Linkedin size={24} />
+          </Text>
+        </Link>
+        <Link href="https://github.com/jmarquesl" passHref>
+          <Text>
+            <Github size={24} />
+          </Text>
+        </Link>
       </XStack>
     </XStack>
   </YStack>
