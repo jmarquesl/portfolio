6025feb00bead1a7fac211cf9d3247c19a483c86
Jordi Marqués Llaberia <jordi.marquesllaberia@gmail.com>
2025-02-16 02:06:25 +0100

    Change footer alignment

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,10 +4,12 @@ import Link from 'next/link';
 
 const Footer = () => (
   <YStack p="$4" ai="center" mt="$6" bg="$background" shadowColor="$shadowColor" shadowRadius="$2" shadowOffset={{ width: 0, height: 2 }}>
-    <XStack gap="$4" mt="$3">
-      <Text fontSize="$4" textAlign="center">GNU GENERAL PUBLIC LICENSE Version 3</Text>
-      <Link href="https://linkedin.com" passHref><Button size="$4" variant="outlined">LinkedIn</Button></Link>
-      <Link href="https://github.com" passHref><Button size="$4" variant="outlined">GitHub</Button></Link>
+    <XStack gap="$4" mt="$3" justifyContent="space-between" width="100%">
+      <Text fontSize="$4" textAlign="left" alignSelf="center">GNU GENERAL PUBLIC LICENSE Version 3</Text>
+      <XStack gap="$4">
+      <Link href="https://linkedin.com" passHref><Button size="$4" variant="outlined" alignSelf="center">LinkedIn</Button></Link>
+      <Link href="https://github.com" passHref><Button size="$4" variant="outlined" alignSelf="center">GitHub</Button></Link>
+      </XStack>
     </XStack>
   </YStack>
 );
