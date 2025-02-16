8d8add3f42484ae62ef8ace4f7f89ad1b5f3e39a
Jordi Marqués Llaberia <jordi.marquesllaberia@gmail.com>
2025-02-16 08:36:10 +0100

    fixed index

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,22 +3,24 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <YStack f={1} ai="center" jc="center" h="100vh" p="$4" gap="$4" alignItems="center">
-      <YStack ai="center" jc="center" pt="$4" pb="$4" alignItems="center">
-        <XStack gap="$4" ai="center" jc="center" alignItems="center">
+    <YStack f={1} ai="center" w="100%" p="$4" gap="$6">
+
+      {/* Sección de presentación */}
+      <YStack maxWidth="90%%" p="$4">
+        <XStack gap="$4" ai="center">
           <Avatar circular size="$10">
-            <Avatar.Image
-              src="/static/images/profile.jpg"
-            />
+            <Avatar.Image src="/static/images/profile.jpg" />
             <Avatar.Fallback backgroundColor="$backgroundFocus" />
           </Avatar>
-          <YStack marginLeft="&6" maxWidth="100%" flexWrap="wrap" jc="center">
-            <Text fontSize="$10" fontWeight="bold" wordWrap="break-word" whiteSpace="pre-wrap">Jordi Marqués Llaberia</Text>
+          <YStack marginLeft="&6" maxWidth="80%" flexWrap="wrap" jc="center">
+            <Text fontSize="$10" fontWeight="bold" wordWrap="break-word">Jordi Marqués Llaberia</Text>
             <Text fontSize="$6">QA and Agile Enthusiast</Text>
           </YStack>
         </XStack>
       </YStack>
-      <YStack w="90%" maxWidth={700} p="$4" borderRadius="$4">
+
+      {/* Texto de presentación */}
+      <YStack w="90%" maxWidth={700} p="$4">
         <Text fontSize="$8" fontWeight="bold" textAlign="center">
           Ingeniero de formación y apasionado por el desarrollo de software y las metodologías ágiles.
         </Text>
@@ -29,8 +31,9 @@ export default function Home() {
           Creo firmemente que, para que un equipo ágil funcione, debe enfocarse en fomentar la mejora continua y permitir que sus miembros se adapten a los desafíos, logrando así soluciones sólidas y alineadas con las necesidades del negocio, siempre colocando a las personas que hacen crecer el software en el centro.
         </Text>
       </YStack>
-      
-      <YStack w="90%" maxWidth={600} p="$4" borderRadius="$4">
+
+      {/* Frase inspiracional */}
+      <YStack w="90%" maxWidth={600} p="$4">
         <Text fontSize="$7" fontWeight="bold" textAlign="center">
           "El optimismo es un riesgo laboral de la programación; el feedback es el tratamiento"
         </Text>
