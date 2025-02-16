4dc487c7b127bb10ed666bb7f8defc539ba45e32
Jordi Marqués Llaberia <jordi.marquesllaberia@gmail.com>
2025-02-16 11:59:16 +0100

    Fixed section spacing

--- components/Section.tsx
+++ components/Section.tsx
@@ -2,7 +2,9 @@ import { YStack, Text, XStack, Button } from 'tamagui';
 
 export const Section = ({ title, children }: { title: string, children: React.ReactNode }) => (
   <YStack>
+    <YStack gap="$3">
     <Text fontSize="$8" fontWeight="bold">{title}</Text>
     {children}
+    </YStack>
   </YStack>
 );
