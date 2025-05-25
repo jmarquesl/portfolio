2564c34da225ae94a304b68acfb0fb8b50bd5a1b
Jordi Marqués Llaberia <jordi.marquesllaberia@gmail.com>
2025-05-26 00:18:48 +0200

    Added shadow and animation to hamburger menu

--- components/HamburgerMenu.tsx
+++ components/HamburgerMenu.tsx
@@ -10,10 +10,6 @@ export default function HamburgerMenu() {
     setIsOpen(!isOpen);
   };
 
-  const handleClose = () => {
-    setIsOpen(false);
-  };
-
   return (
     <Popover open={isOpen} onOpenChange={setIsOpen}>
       <Popover.Trigger onClick={handleToggle}>
@@ -21,9 +17,19 @@ export default function HamburgerMenu() {
           <Menu size="24" />
         </Text>
       </Popover.Trigger>
-      <Popover.Content padding="$4" backgroundColor="$background" borderRadius="$4" onClick={handleClose}>
+      <Popover.Content
+        padding="$4"
+        backgroundColor="$background"
+        borderRadius="$4"
+        onClick={handleToggle}
+        animation="fast"
+        enterStyle={{ opacity: 0, scale: 0.95 }}
+        exitStyle={{ opacity: 0, scale: 0.95 }}
+        elevation="$4"
+        style={{ boxShadow: '0 4px 24px rgba(0,0,0,0.15)' }}
+      >
         <YStack space="$3">
-          <LinkList/>
+          <LinkList />
         </YStack>
       </Popover.Content>
     </Popover>
